d40ef663785213ff65fcbfc316c6d16349572b6a
yadoch <yadoch@gmail.com>
2017-12-01 06:14:29 +0800

    完成ViewPage 切換

--- app/src/main/java/tw/com/abc/myonlyviewpage/MainActivity.java
+++ app/src/main/java/tw/com/abc/myonlyviewpage/MainActivity.java
@@ -50,12 +50,17 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public boolean isViewFromObject(View view, Object object) {
-            return false;
+            // ViewPager 做一些狀態的改變時，為了確認其框架底下的子 View 是否是需被改變的對象，
+            // 會一個一個取出現有的子 View 並和其內的 mItems 頁面陣列一個一個比對，
+            // 其中會呼叫此方法來判斷子 View 是否為對應的頁面。
+            // 如果覺得複雜較難理解，只需一直採用如下寫法就可以。
+            return  view == object;
+            //return false;
         }
         // 另外新加入
         @Override
         public Object instantiateItem(ViewGroup container, int position) {
-
+            //傳回畫面處理
             View view = views.get(position);
             viewPager.addView(view);
             return view;
